8856a5c013688356fbca9c9ff0bcec86a04e964b
sparta13372 <121908207+sparta13372@users.noreply.github.com>
2023-01-19 15:39:52 -0500

    Initial commit

---
+++ Personal Composition/js/app.js
@@ -0,0 +1,56 @@
+
+function setup () {
+    createCanvas (1920,1080);
+}
+
+function draw () {
+    
+    fill(15, 22, 46)
+}
+
+function draw () {
+    fill(15, 22, 46)
+    strokeWeight(10)
+    rect(1, 600, 1920, 200) //road
+    strokeWeight(50);
+    fill (91, 106, 107)
+    circle(960,540,100); //tire 1
+    strokeWeight(50);
+    fill (91, 106, 107);
+    circle(760,540,100); //tire 2
+    fill(24, 18, 181)
+    strokeWeight(10)
+    rect (710,415, 300, 80)// bottom car
+    rect(710,350,200,80) // top car
+    strokeWeight(5)
+    fill(227, 235, 5)
+    rect(5, 693, 75, 20) //road line
+    strokeWeight(5)
+    fill(227, 235, 5)
+    rect(200, 693, 75, 20)
+    strokeWeight(5)
+    fill(227, 235, 5)
+    rect(400, 693, 75, 20)
+    strokeWeight(5)
+    fill(227, 235, 5)
+    rect(600, 693, 75, 20)
+    strokeWeight(5)
+    fill(227, 235, 5)
+    rect(800, 693, 75, 20)
+    strokeWeight(5)
+    fill(227, 235, 5)
+    rect(1000, 693, 75, 20)
+    strokeWeight(5)
+    fill(227, 235, 5)
+    rect(1200, 693, 75, 20)
+    strokeWeight(5)
+    fill(227, 235, 5)
+    rect(1400, 693, 75, 20)
+    strokeWeight(5)
+    fill(227, 235, 5)
+    rect(1600, 693, 75, 20)
+    strokeWeight(5)
+    fill(227, 235, 5)
+    rect(1800, 693, 75, 20)
+}
+
