9eba97c9e0a1e5024d9529438244a5995b5f90eb
sparta13372 <121908207+sparta13372@users.noreply.github.com>
2023-01-25 11:38:04 -0500

    fuck

--- Animation/app.js
+++ Animation/app.js
@@ -1,6 +1,31 @@
 //This is the plan for the animation
-//1. create object
-//2. Give object design, aka, color/strokeWeight/etc.
-//3. give object a change in position
-//4. use an IF statement to change the direction of poisition change
+//1. create canvas
+//2. create circle, give simple design
+//3. make circle follow the mouse
+//4. use an IF statement to change the color of the circle based on poisition
 
+
+function setup() {
+  createCanvas(400, 300);
+  
+}
+
+function draw(){
+  background(0,0,0,20)
+  circleo(mouseX,mouseY)
+}
+
+function circleo(x,y) {
+    if(x>200) {
+        fill(255,255,0) 
+    } else {
+        fill(10,50,100)
+    }
+
+  noStroke()
+    //fill(250,250,100)
+  circle(x,y,50)
+  
+  
+  
+}
