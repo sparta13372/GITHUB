e65db490e408dcb0f91f1027c18d37035d7f04a9
sparta13372 <121908207+sparta13372@users.noreply.github.com>
2023-01-26 16:31:24 -0500

    try 4

--- Animation/java/counter.js
+++ Animation/java/counter.js
@@ -1,9 +1,12 @@
+//set value
 let x = 0
 
+//create canvas, dont know if neccesary for this
 function setup () {
     createCanvas(400,400)
 }
 
+//use console.log to increae value on draw
 function draw () {
     x = x + 1
     console.log(x)
--- Animation/java/worldWarp.js
+++ Animation/java/worldWarp.js
@@ -1,14 +1,20 @@
+//set value
 let x = 0
 
+
+//create canvas for ball
 function setup () {
     createCanvas(800,600)
 }
 
+//draw circle that goes from one edge to another
 function draw () {
     background("#000")
     circle(x,100,100)
     x = x + 5
     console.log(x)
+
+    //reset position of circle
     if (x>800) {
         x = 0   
     }
